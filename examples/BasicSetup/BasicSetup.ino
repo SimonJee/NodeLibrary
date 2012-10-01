fb9adb4292d8eb94dac7f15041bc02a24535055f
SimonJee <simon@fesselndes-hamburg.de>
2012-10-01 15:47:10 +0200

    Changes to BasicSetup example

---
+++ examples/BasicSetup/BasicSetup.ino
@@ -0,0 +1,26 @@
+/*
+	NodeLibrary basic setup
+
+	Demonstration of the NodeLibrary.                                
+
+	The circuit:
+	* Uses JeeNode from JeeLabs.org
+
+	Created 1 October 2012
+	By SimonJee
+	Modified 1 October 2012
+	By SimonJee
+
+	http://github.com/SimonJee/NodeLibrary
+*/
+
+#include <Node.h>
+
+void setup() {
+  // Assign a human readable identifier to each node. 
+  Node::setName("BasicNode");
+}
+
+void loop() {
+  
+}
